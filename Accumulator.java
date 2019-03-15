c02a8e1a755bc405a9ce550d0714cbebf16f9370
Akash Das <adas00@stuy.edu>
2019-03-15 13:18:43 -0400

    add Accumulator

---
+++ Accumulator.java
@@ -0,0 +1,31 @@
+/**
+  Represent accumulating data from the elements in a
+  List_inArraySlots that are of a particular type.
+ */
+
+public class Accumulator {
+
+    /**
+      @return the concatenation of all the Strings
+      in the \list that begin with \prefix,
+      each followed by a space.
+     */
+    String catElementsStartingWith(
+        List_inArraySlots list
+      , String prefix
+      ) {
+        String result;
+        result = "change me";
+        return result; 
+    }
+
+
+    // /**
+      // @return a list of each of the Double elements
+      // from the \list whose value is "finite".
+     // */
+    // public List_inArraySlots finites(
+        // List_inArraySlots list
+      // ) {
+    // }
+}
