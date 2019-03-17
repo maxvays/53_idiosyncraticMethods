e1c8c82a4326cdefe9a5d0a50da88c3c86b6a7ec
Max Vaysburd <max.vays@gmail.com>
2019-03-16 20:31:58 -0400

    Add stumbling block and workaround 0

--- Accumulator.java
+++ Accumulator.java
@@ -10,14 +10,40 @@ public class Accumulator {
       in the \list that begin with \prefix,
       each followed by a space.
      */
-    static String catElementsStartingWith(
-        List_inArraySlots list
-      , String prefix
-      ) {
-        String result;
-        result = "change me";
-        return result;
-    }
+  //   static String catElementsStartingWith(
+  //         List_inArraySlots list
+  //         , String prefix
+  //         ) {
+  //     String result;
+  //     for(int i = 0; i < list.size(); i++){
+  //       if(list.get(i).startsWith("pi")) result += i;
+  //     }
+  //     return result;
+  // }
+
+/*
+ predicted error message: Object i does not have method startsWith
+
+ actual error message: cannot find symbol
+
+ */
+
+ /* 4.  Workaround 0
+    A programmer should expect to be able to work around the
+    stumbling block somehow, because
+    the JVM knows the type of an element.
+
+    Java's instanceOf operator identifies the type
+    of an element to the JVM.
+  */
+
+    //     List_inArraySlots list
+    //   , String prefix
+    //   ) {
+    //     String result;
+    //     result = "change me";
+    //     return result;
+    // }
 
 
     // /**
